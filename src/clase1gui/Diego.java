a7240fcb37e9e87062c402d850b61f155e29a685
PERSONAL <PERSONAL@DESKTOP-Q1JD984>
2022-06-12 18:02:09 -0500

    creacion de interfaz

---
+++ src/clase1gui/Diego.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase1gui;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Diego {
+    public void numeros(){
+        System.out.println("mi primer cambio");
+    }
+}
